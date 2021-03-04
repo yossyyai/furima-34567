3fac82f4c28b8749fd187c1cebdc257c66741f73
yossyyai <yoshiyukiui.ocean@gmail.com>
2021-03-04 23:09:13 +0900

    factorybotの作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    first_name            { person.first.kanji}
+    last_name             { person.last.kanji}
+    first_name_kana       { person.first.katakana}
+    last_name_kana        {person.last.katakana}
+    nickname              {Faker::Name.initials(number: 2)}
+    birth_date            {Faker::Date.backward}
+    email                 {Faker::Internet.free_email}
+    password = "aaa111"
+    password               {password}
+    password_confirmation  {password}
+  end
+end
