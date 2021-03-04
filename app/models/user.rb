44c3c50b1829cf69c91cd4beb549ed435cd0c5ce
yossyyai <yoshiyukiui.ocean@gmail.com>
2021-03-05 00:01:01 +0900

    エラーメッセージの重複の解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,22 +6,18 @@ class User < ApplicationRecord
 
   with_options presence: true do
   validates :nickname
-  validates :first_name
-  validates :last_name
-  validates :first_name_kana
-  validates :last_name_kana
   validates :birth_date
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers', allow_blank: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters', allow_blank: true} do
     validates :first_name
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters', allow_blank: true} do
     validates :first_name_kana
     validates :last_name_kana
   end
