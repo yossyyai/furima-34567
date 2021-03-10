380388bcdd0cc09deb86c9d6e42e6710cbcfc8b5
yossyyai <yoshiyukiui.ocean@gmail.com>
2021-03-10 17:09:55 +0900

    createメソッド作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(article_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:item).permit(:name,:description,:category_id,:status_id,:charges_id,:prefectures_id,:ship_date_id,:price,:user_id)
+  end
 end
