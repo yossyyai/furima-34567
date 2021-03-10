fff480515253200fe91afa4f40087d0ee4fd486f
yossyyai <yoshiyukiui.ocean@gmail.com>
2021-03-10 17:01:47 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,18 @@ class Item < ApplicationRecord
   belongs_to :prefectures
   belongs_to :ship_date
 
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
+ 
+  with_options numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :status_id
+    validates :charges_id
+    validates :prefectures_id
+    validates :ship_date_id
+  end
+
 end
