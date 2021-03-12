0a2005e1ed2ad7668bd8fc4a1e12c701a84e1ed4
yossyyai <yoshiyukiui.ocean@gmail.com>
2021-03-12 12:01:07 +0900

    javascriptの修正

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -2,7 +2,7 @@ function calc (){
   var price = document.getElementById('item-price')
   price.addEventListener('input', () => {
     price = document.getElementById('item-price').value;
-    const tax = parseInt(price) * 0.1;
+    const tax = parseInt(price * 0.1);
     const profit = price - tax;
     document.getElementById('add-tax-price').innerHTML = tax;
     document.getElementById('profit').innerHTML = profit;
