2edde06ddf1bcc5f33bfc068916c309b56414554
yossyyai <yoshiyukiui.ocean@gmail.com>
2021-03-12 15:07:19 +0900

    商品のインスタンス変数の定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-
+    @items = Item.all
   end
   def new
     @item = Item.new
