435e8972c0020d44431f89d280de75f2ce5673cc
yossyyai <yoshiyukiui.ocean@gmail.com>
2021-03-17 20:20:05 +0900

    不正防止アクセスのため、条件分岐にupdateの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :move_index, only: [:edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :set_edit, only: :edit
+  before_action :set_edit, only: [:edit, :update]
 
 
   def index
