eb2c303f38ad4bdd4f606423ef574e35c6100564
yossyyai <yoshiyukiui.ocean@gmail.com>
2021-03-18 16:40:05 +0900

    バリデーションの編集

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,13 +4,16 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)", allow_blank: true }
-    validates :prefectures_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :municipality
     validates :house_number
     validates :phone_number, format: { with: /\A[0-9０-９]+\z/ , allow_blank: true },length: { maximum: 11 }
     validates :item_id
     validates :user_id
     validates :token
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :prefectures_id
   end
 
 
